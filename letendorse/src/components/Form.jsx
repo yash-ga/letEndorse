e83b20c109b1522012869b664240b9fa4309c349
Yash Garg <yash_fw10_103@masai.school>
2022-02-16 12:07:15 +0530

    commiting top functionality

--- letendorse/src/components/Form.jsx
+++ letendorse/src/components/Form.jsx
@@ -5,6 +5,10 @@ export const Form=()=>
     const [name,setName]=useState("")
     const [idea,setIdea]=useState("")
     const [age,setAge]=useState("")
+    const [product,setProduct]=useState("")
+    const [offer,setOffer]=useState("")
+    const [secondary,setSecondary]=useState("")
+    const [years,setYears]=useState("")
     const getName=(e)=>{
         setName(e.target.value)
     }
@@ -21,12 +25,29 @@ export const Form=()=>
             </select>
             <h3>age of est</h3>
             <input type="text" id="age" onChange={(e)=>{setAge(e.target.value)}} />
-
+            <h4>primary products/services offered</h4>
+            <input type="text"  onChange={(e)=>{setProduct(e.target.value)}}/>
+            <h5>offered to</h5>
+            <select name="" id="" onChange={(e)=>{setOffer(e.target.value)}}>
+                <option value="wholesaler">wholesaler</option>
+                <option value="distributors">distributors</option>
+                <option value="rilerseta">retailer</option>
+                <option value="consumer">consumer</option>
+            </select>
+            <h5>secondary products/services/offered</h5>
+            <input type="text" placeholder="secondary services offered" onChange={(e)=>{setSecondary(e.target.value)}} />
+            <h5>years of exp</h5>
+            <input type="text" placeholder="year of exp" onChange={(e)=>{setYears(e.target.value)}} name="" id="" />
+             
             </article>
             <article className="right">
             <p>{name}-is looking for xyz</p>
             <p>{idea}</p>
             <p>{age}</p>
+            <p>{product}</p>
+            <p>{offer}</p>
+            <p>{secondary}</p>
+            <p>{years}</p>
 
             </article>
         </div>
